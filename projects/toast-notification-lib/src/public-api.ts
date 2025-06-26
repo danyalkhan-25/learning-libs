f8fcfa2b03d035270e64527281bf3067e1071ed6
Danyal Khan <danyal@platformcommons.com>
2025-06-26 19:12:33 +0530

    update toast.service public-api

--- projects/toast-notification-lib/src/public-api.ts
+++ projects/toast-notification-lib/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of toast-notification-lib
  */
 
-export * from './lib/toast-notification-lib.service';
+export * from './lib/toast.service';
 export * from './lib/toast/toast.component';
 export * from './lib/toast-notification-lib.module';
